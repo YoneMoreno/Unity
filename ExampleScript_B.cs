14fd5f4c23ef89bdae362ddb140918762676ee54
YonePC <YonePC@YONE>
2017-10-08 17:24:41 +0100

    Learnt for, while do while for eachs and switch in c#

--- ExampleScript_B.cs
+++ ExampleScript_B.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class ExampleScript_B : MonoBehaviour {
 	private int enemyDistance = 0;
+	private int enemyCount = 10;
+
+	private string[] enemies = new string[5];
+	private int weaponId = 0;
 
 	// Use this for initialization
 	void Start () {
-		
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyUp("space")) {
-			EnemySearch ();
+			//EnemySearch ();
+			//EnemyDestruction();
+			//EnemyScan();
+			//EnemyRoster();
+			WeaponSearch();
 		}
 	}
 
@@ -31,4 +40,59 @@ public class ExampleScript_B : MonoBehaviour {
 			}
 		}
 	}
+
+	void EnemyDestruction(){
+		while (enemyCount > 0) {
+			print ("There is an enemy Lets destroy it");
+			enemyCount--;
+		}
+	}
+
+	void EnemyScan(){
+		bool isAlive = false;
+
+		do {
+			print("Scanning for enemies");
+		} while (isAlive == true);
+	}
+
+	void EnemyRoster(){
+		enemies [0] = "orc";
+		enemies [1] = "dragon";
+		enemies [2] = "elf";
+		enemies [3] = "baby";
+		enemies [4] = "clown";
+
+		foreach (string enemy in enemies) {
+			print (enemy);
+		}
+	}
+
+	void WeaponSearch(){
+		weaponId = Random.Range (0, 10);
+		switch (weaponId) {
+		case 1:
+			print("You found a sword");
+			break;
+		case 2:
+			print("You found an axe");
+			break;
+		case 3:
+			print("You found a block");
+			break;
+		case 4:
+			print("You found a dagger");
+			break;
+		default:
+			print("You found a nothing ;)");
+			break;
+
+		}
+	
+	}
+
 }
+
+
+
+
